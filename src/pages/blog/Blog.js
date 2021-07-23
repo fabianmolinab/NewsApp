fda9ef6d23f2c3566c44207c543818a7fd771514
fabianmolinab <molinafabian11b@gmail.com>
2021-07-22 23:49:19 -0500

    Cambiando el Router de la pagina del Blog

--- src/pages/blog/Blog.js
+++ src/pages/blog/Blog.js
@@ -1,32 +1,24 @@
 import React from 'react';
-import {
-  BrowserRouter as Redirect, Route, Switch,
-} from 'react-router-dom';
-import { Logo } from '../../components/atoms/Logo';
-
-import { NavBar } from '../../components/atoms/NavBar';
-import { Botones } from '../../components/molecules/Botones';
+import { BrowserRouter as Redirect, Route, Switch } from 'react-router-dom';
 import { SeccionDestacados } from './SeccionDestacados';
 import { SeccionNuevo } from './SeccionNuevo';
+import { HeaderBlog } from '../../components/molecules/HeaderBlog';
 
 export const Blog = () => {
 
   return (
-    <>
-      <header>
-        <NavBar />
-        <Logo />
-        <Botones />
-      </header>
-      <div>
-        <Switch>
-          <Route exact path="/blog" component={Blog} />
-          <Route exact path="/blog/destacados" component={SeccionDestacados} />
-          <Route exact path="/blog/nuevo" component={SeccionNuevo} />
-          <Redirect to="/blog" />
-        </Switch>
-      </div>
-    </>
+      <>
+        <HeaderBlog/>
+        <div>
+          <Switch>
+            <Route exact path="/blog" component={ Blog }/>
+            <Route exact path="/blog/destacados"
+                   component={ SeccionDestacados }/>
+            <Route exact path="/blog/nuevo" component={ SeccionNuevo }/>
+            <Redirect to="/blog"/>
+          </Switch>
+        </div>
+      </>
   );
 };
 
