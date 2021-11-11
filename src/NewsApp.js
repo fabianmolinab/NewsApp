128a48be3b18c1f36def2ee2da32a859aa00f912
fabianmolinab <molinafabian11b@gmail.com>
2021-11-11 17:23:32 -0500

    Solo estilos

--- src/NewsApp.js
+++ src/NewsApp.js
@@ -1,16 +1,16 @@
 import React from 'react';
+import { NewContextProvider } from './context/NewContext';
 import { AppRouter } from './routers/AppRouter';
 import { GlobalStyle } from './styles/GlobalStyle';
-import { NewContextProvider } from './context/NewContext';
 
 export const NewsApp = () => {
 
   return (
-      <NewContextProvider>
-        <React.StrictMode>
-          <GlobalStyle/>
-          <AppRouter/>
-        </React.StrictMode>
-      </NewContextProvider>
+    <NewContextProvider>
+      <React.StrictMode>
+        <GlobalStyle />
+        <AppRouter />
+      </React.StrictMode>
+    </NewContextProvider>
   );
 };
